5eb4e9b81a39516991f206c6ce91fba31378da20
Javier Diaz <javier.diazoliviero@gmail.com>
2022-11-17 20:01:19 +0100

    cards array

---
+++ src/components/popupcards/PopUpCards.jsx
@@ -0,0 +1,38 @@
+import img1 from '../../assets/img/San-Valentin-1.png';
+import img2 from '../../assets/img/Pera-limonera-2.png';
+import img3 from '../../assets/img/Sagrada-estrella-3.png';
+import img4 from '../../assets/img/Tarjeta-maneki-1.png';
+
+
+const frases = [
+  {
+    id: 1,
+    price: '12',
+    img: img1,
+    stock: true,
+    category: 'love'
+  },
+  {
+    id: 2,
+    price: '15',
+    img: img2,
+    stock: true,
+    category: 'Friendship'
+  },
+  {
+    id: 3,
+    price: '13',
+    img: img3,
+    stock: true,
+    category: 'Cities'
+  },
+  {
+    id: 4,
+    price: '22',
+    img: img4,
+    stock: true,
+    category: 'Cities'
+  }
+];
+
+export default frases;
