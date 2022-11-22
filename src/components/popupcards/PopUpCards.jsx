8799990510b2760aa62076b08e5e3ce18a5ebf09
Javier Diaz <javier.diazoliviero@gmail.com>
2022-11-22 17:24:32 +0100

    New cards images

--- src/components/popupcards/PopUpCards.jsx
+++ src/components/popupcards/PopUpCards.jsx
@@ -1,8 +1,26 @@
 import img1 from '../../assets/img/San-Valentin-1.png';
 import img2 from '../../assets/img/Pera-limonera-2.png';
 import img3 from '../../assets/img/Sagrada-estrella-3.png';
-import img4 from '../../assets/img/Tarjeta-maneki-1.png';
-
+import img4 from '../../assets/img/ManekiNeko-1.png';
+import img5 from '../../assets/img/Palacio-de-Liria-5.png';
+import img6 from '../../assets/img/Superbrain-6.png';
+import img7 from '../../assets/img/Sonrisas-de-Bombay-7.png';
+import img8 from '../../assets/img/Reikjavik-8.png';
+import img9 from '../../assets/img/Eternals-9.png';
+import img10 from '../../assets/img/Merce-10.png';
+import img11 from '../../assets/img/Mercadillo-Navidad-11.png';
+import img12 from '../../assets/img/Navidad-12.png';
+import img13 from '../../assets/img/Peace-13.png';
+import img14 from '../../assets/img/La-Passio-14.png';
+import img15 from '../../assets/img/Emociones-15.png';
+import img16 from '../../assets/img/Darjeeling-16.png';
+import img17 from '../../assets/img/Eddy-Cifre-17.png';
+import img18 from '../../assets/img/Chimichurri-18.png';
+import img19 from '../../assets/img/Iceland-19.png';
+import img20 from '../../assets/img/Jaumeji-20.png';
+import img21 from '../../assets/img/Limonada-21.png';
+import img22 from '../../assets/img/Cuchara-de-palo-22.png';
+import img23 from '../../assets/img/CeciRosario-23.png';
 
 const cards = [
   {
@@ -32,6 +50,139 @@ const cards = [
     img: img4,
     stock: true,
     category: 'Cities'
+  },
+  {
+    id: 5,
+    price: '18',
+    img: img5,
+    stock: true,
+    category: 'Cities'
+  },
+  {
+    id: 6,
+    price: '15',
+    img: img6,
+    stock: true,
+    category: 'Brand'
+  },
+  {
+    id: 7,
+    price: '10',
+    img: img7,
+    stock: true,
+    category: 'Brand'
+  },
+  {
+    id: 8,
+    price: '10',
+    img: img8,
+    stock: true,
+    category: 'Cities'
+  },
+  {
+    id: 9,
+    price: '35',
+    img: img9,
+    stock: true,
+    category: 'Movies'
+  },
+  {
+    id: 10,
+    price: '15',
+    img: img10,
+    stock: true,
+    category: 'Cities'
+  },
+  {
+    id: 11,
+    price: '13',
+    img: img11,
+    stock: true,
+    category: 'Christmas'
+  },
+  {
+    id: 12,
+    price: '18',
+    img: img12,
+    stock: true,
+    category: 'Christmas'
+  },
+  {
+    id: 13,
+    price: '16',
+    img: img13,
+    stock: true,
+    category: 'Peace'
+  },
+  {
+    id: 14,
+    price: '14',
+    img: img14,
+    stock: true,
+    category: 'Brand'
+  },
+  {
+    id: 15,
+    price: '25',
+    img: img15,
+    stock: true,
+    category: 'Brand'
+  },
+  {
+    id: 16,
+    price: '36',
+    img: img16,
+    stock: true,
+    category: 'Brand'
+  },
+  {
+    id: 17,
+    price: '38',
+    img: img17,
+    stock: true,
+    category: 'Brand'
+  },
+  {
+    id: 18,
+    price: '38',
+    img: img18,
+    stock: true,
+    category: 'Brand'
+  },
+  {
+    id: 19,
+    price: '38',
+    img: img19,
+    stock: true,
+    category: 'Cities'
+  },
+  {
+    id: 20,
+    price: '38',
+    img: img20,
+    stock: true,
+    category: 'Brand'
+  },
+  {
+    id: 21,
+    price: '17',
+    img: img21,
+    stock: true,
+    category: 'Quotes'
+  },
+  {
+    id: 22,
+    price: '14',
+    img: img22,
+    stock: true,
+    category: 'Quotes'
+  },
+  {
+    id: 23,
+    price: '19',
+    img: img23,
+    stock: true,
+    category: 'Brand'
   }
 ];
 
