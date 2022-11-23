8f884094e34a73ed119c0e8b63a9e85f072012f9
Javier Diaz <javier.diazoliviero@gmail.com>
2022-11-23 11:02:17 +0100

    Styled  react icons

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,20 +6,24 @@ import Icon from '../icon/Icon';
 
 export const Footer = () => {
   return (
-        <footer>
-            <div className="footer-links">
-            <a href="https://bit.ly/3GO2UDU">
-                <FaYoutube className="link" />
-            </a>
-            <> </>
-            <a href="https://bit.ly/3V2SNiZ">
-                <FaInstagram className="link" />
-            </a>
-            <> </>
-            <a href="https://bit.ly/3GFYIq6">
-                <FaPinterest className="link" />
-            </a>
-            </div>
+        <footer className='footer-container'>
+            <ul className="footer-list-container">
+              <li className='footer-list-item'>
+                <a href="https://bit.ly/3GO2UDU">
+                  <Icon value={{ className: 'nav-icons', style: { width: '2.5rem' } }}><FaYoutube/></Icon>
+                </a>
+              </li>
+              <li>
+                <a href="https://bit.ly/3V2SNiZ">
+                  <Icon value={{ className: 'nav-icons', style: { width: '1.6rem' } }}><FaInstagram/></Icon>
+                </a>
+              </li>
+              <li>
+                <a href="https://bit.ly/3GFYIq6">
+                  <Icon value={{ className: 'nav-icons', style: { width: '1.6rem' } }}><FaPinterest/></Icon>
+                </a>
+              </li>
+            </ul>
             <p className="footer-text">
               Copyright © by Casa de Chaupi 2022.{" "}
             </p>
