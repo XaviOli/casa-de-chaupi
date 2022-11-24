86fe2684156128c82934bf4c7de959c3a449766f
Javier Diaz <javier.diazoliviero@gmail.com>
2022-11-24 21:04:07 +0100

    More new cards

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import arnau from '../../assets/img/Arnau-5.png';
+import arnau from '../../assets/img/Arnau-cumple-26.png';
 import eternals from '../../assets/img/Eternals-9.png';
-import chimichurri from '../../assets/img/Chimichurri-18.png';
+import liria from '../../assets/img/Palacio-de-Liria-5.png';
 import blob from '../../assets/img/blob.svg';
 import '../home/Home.css'
 
@@ -10,7 +10,7 @@ const Home = () => {
 		<div className="home-container">
 			<img className="home-forefront-image" alt="arnau" src={arnau} />
 			<img className="home-forefront-image" alt="eternals" src={eternals} />
-			<img className="home-forefront-image" alt="chimichurri" src={chimichurri} />
+			<img className="home-forefront-image" alt="chimichurri" src={liria} />
 			<div className="home-background">
 				<img className="home-background-image" alt="blob" src={blob} />
 			</div>
