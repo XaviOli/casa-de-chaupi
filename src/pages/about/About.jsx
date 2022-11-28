5d385d597e5fcf6aa6b0b931e7150ab1cf2bdaf5
Javier Diaz <javier.diazoliviero@gmail.com>
2022-11-28 18:27:50 +0100

    About new bg

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,24 +1,38 @@
 import React from 'react'
-import manualidades from '../../assets/img/pexels-manualidades.jpg';
-import tijeras from '../../assets/img/pexels-tijeras.jpg';
+// import manualidades from '../../assets/img/pexels-manualidades.jpg';
+// import tijeras from '../../assets/img/pexels-tijeras.jpg';
+
+import aboutbg from '../../assets/img/About-gb-40.jpg';
 import '../about/About.css';
+
+// const About = () => {
+//   return (
+//     <div className="about-container">
+//       <div className="about-main">
+//         <img className="about-img-container" src={manualidades} alt='Manualidades con cartulinas' />
+//         <div>
+//           <p className='about-text-container'>
+//           Lorem ipsum dolor sit amet consectetur adipiscing elit arcu leo nascetur 
+//           nam dui, magna dignissim vivamus velit lobortis porta ridiculus porttitor 
+//           ornare condimentum rhoncus praesent, aptent cubilia quam taciti lacus 
+//           facilisi hendrerit parturient fringilla sodales placerat.
+//           </p>
+//         </div>
+//         <div>
+//           <img className="about-img-container" src={tijeras} alt='Manualidades con tijeras' />
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default About;
 
 const About = () => {
   return (
     <div className="about-container">
       <div className="about-main">
-        <img className="about-img-container" src={manualidades} alt='Manualidades con cartulinas' />
-        <div>
-          <p className='about-text-container'>
-          Lorem ipsum dolor sit amet consectetur adipiscing elit arcu leo nascetur 
-          nam dui, magna dignissim vivamus velit lobortis porta ridiculus porttitor 
-          ornare condimentum rhoncus praesent, aptent cubilia quam taciti lacus 
-          facilisi hendrerit parturient fringilla sodales placerat.
-          </p>
-        </div>
-        <div>
-          <img className="about-img-container" src={tijeras} alt='Manualidades con tijeras' />
-        </div>
+        <img className="about-img-container" src={aboutbg} alt='background About' />
       </div>
     </div>
   )
